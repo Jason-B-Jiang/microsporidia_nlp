1e595174ddf99118e51680deece39233dfb511a3
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2022-05-24 14:53:16 -0400

    Evaluate polar tube coil extraction

--- src/2_predict_traits/evaluate_extracted_traits.R
+++ src/2_predict_traits/evaluate_extracted_traits.R
@@ -16,7 +16,13 @@ library(tidyverse)
 
 ## Evaluate polar tube coil predictions
 
-format_coil_range <- function(pt_coils_range) {
+WORDS_TO_DIGITS <- list('one' = 1, 'two' = 2, 'three' = 3, 'four' = 4,
+                        'five' = 5, 'six' = 6, 'seven' = 7, 'eight' = 8,
+                        'nine' = 9, 'ten' = 10)
+
+PT_COIL_RANGE <- '\\d{1,2}\\.?\\d?( to | or | and | ?- ?| ?– ?| ?— ?|\\/)?\\d*\\.?\\d?'
+
+date_to_range <- function(pt_coils_range) {
   # ---------------------------------------------------------------------------
   # Turn ranges that Excel turned into dates, back into ranges
   # ---------------------------------------------------------------------------
@@ -32,13 +38,87 @@ format_coil_range <- function(pt_coils_range) {
   return(pt_coils_range)
 }
 
+turn_number_words_to_digits <- function(pt_coil_pred) {
+  # ---------------------------------------------------------------------------
+  # Replace number words in extracted polar tube coil data with digits
+  # ex: 'five to six coils' -> '5 to 6 coils'
+  # ---------------------------------------------------------------------------
+  # get individual coil predictions
+  pt_coil_pred <- str_split(pt_coil_pred, ' \\|\\|\\| ')[[1]]
+  
+  for (i in 1 : length(pt_coil_pred)) {
+    pred <- str_split(pt_coil_pred[i], ' ')[[1]]  # split into words
+    
+    for (j in 1 : length(pred)) {
+      if (tolower(pred[j]) %in% names(WORDS_TO_DIGITS)) {
+        pred[j] <- WORDS_TO_DIGITS[[tolower(pred[j])]]
+      }
+    }
+    
+    pt_coil_pred <- replace(pt_coil_pred, i, str_c(pred, collapse = ' '))
+  }
+  
+  return(str_c(pt_coil_pred, collapse = ' ||| '))
+}
+
+
+extract_pt_coil_range <- function(pt_coil_pred) {
+  return(
+    str_c(sapply(str_split(pt_coil_pred, ' \\|\\|\\| ')[[1]],
+                         function(s) {str_extract(s, PT_COIL_RANGE)}),
+                        collapse = ' ||| ')
+  )
+}
+
+
+convert_pt_coil_ranges_to_medians <- function(pred_pt_coil_formatted) {
+  pt_coil_preds <- str_split(pred_pt_coil_formatted, ' \\|\\|\\| ')[[1]]
+  
+  for (i in 1 : length(pt_coil_preds)) {
+    # get average/median of a range of polar tube coils
+    pt_coil_preds[i] <-
+      mean(
+        as.numeric(str_split(pt_coil_preds[i], '( to | or | and | ?- ?| ?– ?| ?— ?|\\/)')[[1]])
+        )
+  }
+  
+  return(str_c(pt_coil_preds, collapse = '; '))
+}
+
+
+get_pt_coil_precision <- function(pt_coils_avg, pred_pt_coil_avg) {
+  pt_coils_avg <- str_split(pt_coils_avg, '; ')[[1]]
+  pred_pt_coil_avg <- str_split(pred_pt_coil_avg, '; ')[[1]]
+  
+  true_pos <- length(pred_pt_coil_avg[pred_pt_coil_avg %in% pt_coils_avg])
+  false_pos <- length(pred_pt_coil_avg[!(pred_pt_coil_avg %in% pt_coils_avg)])
+  
+  return(true_pos / (true_pos + false_pos))
+}
+
+
+get_pt_coil_recall <- function(pt_coils_avg, pred_pt_coil_avg) {
+  pt_coils_avg <- str_split(pt_coils_avg, '; ')[[1]]
+  pred_pt_coil_avg <- str_split(pred_pt_coil_avg, '; ')[[1]]
+  
+  true_pos <- length(pred_pt_coil_avg[pred_pt_coil_avg %in% pt_coils_avg])
+  false_neg <- length(pt_coils_avg[!(pt_coils_avg %in% pred_pt_coil_avg)])
+  
+  return(true_pos / (true_pos + false_neg))
+}
+
 
 pt_coil_preds <- read_csv('../../results/microsp_pt_predictions.csv') %>%
   rowwise() %>%
   mutate(pt_coils_range = ifelse(!is.na(pt_coils_range),
-                                        format_coil_range(pt_coils_range),
-                                        NA)) %>%
-  filter(!is.na(pred_pt_coil) | !is.na(pt_coils_range) | !is.na(pt_coils_avg))
+                                        date_to_range(pt_coils_range),
+                                        NA),
+         pred_pt_coil_formatted = extract_pt_coil_range(turn_number_words_to_digits(pred_pt_coil)),
+         pred_pt_coil_avg = convert_pt_coil_ranges_to_medians(pred_pt_coil_formatted)) %>%
+  filter(!is.na(pred_pt_coil) | !is.na(pt_coils_range) | !is.na(pt_coils_avg)) %>%
+  mutate(precision = get_pt_coil_precision(pt_coils_avg, pred_pt_coil_avg),
+         recall = get_pt_coil_recall(pt_coils_avg, pred_pt_coil_avg))
+  
 
 ################################################################################
 
