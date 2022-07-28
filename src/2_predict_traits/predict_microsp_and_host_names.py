9121c8c3a6ec09b6c608e29db50bce5cd51f3d28
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2022-07-28 11:32:42 -0400

    Continue microsporidia and host predictions from text

--- src/2_predict_traits/predict_microsp_and_host_names.py
+++ src/2_predict_traits/predict_microsp_and_host_names.py
@@ -3,7 +3,7 @@
 # Predict microsporidia species names + hosts from paper titles + abstracts
 #
 # Jason Jiang - Created: 2022/05/19
-#               Last edited: 2022/07/27
+#               Last edited: 2022/07/28
 #
 # Mideo Lab - Microsporidia text mining
 #
@@ -20,16 +20,16 @@ import spacy
 from spacy.matcher import PhraseMatcher
 from pathlib import Path
 from pygbif import species
-from typing import Tuple
+from typing import Tuple, List
 
 ################################################################################
 
 ## Initialize language models
 
 # Initialize Taxonerd species entity predictor
-taxonerd = TaxoNERD(model="en_ner_eco_biobert",  # use more accurate model
+taxo = TaxoNERD(model="en_ner_eco_biobert",  # use more accurate model
                     prefer_gpu=False,
-                    with_abbrev=False)
+                    with_abbrev=True)
 
 # Initialize spaCy medium pipeline
 nlp = spacy.load('en_core_web_md')
@@ -37,6 +37,8 @@ nlp = spacy.load('en_core_web_md')
 ################################################################################
 
 ## Define regex patterns for detecting new species names
+
+SPECIES = r'[A-Z][a-z]+ [a-z]+( [a-z]+)?'
 
 # all microsporidia genus names from NCBI
 MICROSP_GENUSES = pd.read_csv(
@@ -50,8 +52,8 @@ NEW_SPECIES_INDICATORS = \
 
 NEW_SPECIES_INDICATORS =  r'{}'.format('(' + '|'.join(NEW_SPECIES_INDICATORS) + ')')
  
-# final regex pattern for new microspridia species in text
-NEW_SPECIES = r'{}'.format(
+# final regex pattern for microspridia species in text
+MICROSP_SPECIES = r'{}'.format(
     f"(({'|'.join(MICROSP_GENUSES)}) [A-Z]?[a-z]+|{SPECIES}(?=,?{NEW_SPECIES_INDICATORS}))"
     )
 
@@ -72,12 +74,12 @@ def main() -> None:
     # add in column for Microsporidia species, as flagged by entity ruler
     # add in column for host species, which is any taxonomic entity detected by
     # TaxoNERD and is not a Microsporidia species
-    microsp_data = microsp_data.assign(
-        pred_microsporidia = lambda x: x['title_abstract'].map(
-            lambda s: get_microsporidia_from_text(s),
-            na_action='ignore'
-        )
-    )
+    # microsp_data = microsp_data.assign(
+    #     pred_microsporidia = lambda x: x['title_abstract'].map(
+    #         lambda s: get_microsporidia_from_text(s),
+    #         na_action='ignore'
+    #     )
+    # )
 
     # write modified microsp_data dataframe to results folder
     microsp_data[['title_abstract', 'species', 'pred_microsporidia',
@@ -97,16 +99,58 @@ def get_spacy_cached_text(txt: str) -> spacy.tokens.doc.Doc:
     return SPACY_CACHE[txt]
 
 
+def get_microsporidia_from_doc(doc: spacy.tokens.doc.Doc) -> \
+    List[spacy.tokens.span.Span]:
+    """Return a list of spans corresponding to putative Microsporidia species
+    mentioned in a spaCy document.
+    
+    Return an empty list if no Microsporidia species are found.
+    """
+    microsp_matches = []
+    for match in re.finditer(MICROSP_SPECIES, doc.text):
+        start, end = match.span()
+        span = doc.char_span(start, end)
+
+        if span is not None:
+            microsp_matches.append(span)
+
+    return microsp_matches
+
+
+def get_taxonerd_taxons(doc: spacy.tokens.doc.Doc) -> List[spacy.tokens.span.Span]:
+    """Return a list of spans corresponding to predicted taxons by TaxoNERD
+    from document.
+    """
+    pred_taxons = taxo.find_in_text(doc.text)
+
+    # filter out any rows in pred_taxons with 'microsporidium' or 'microsporidia'
+    pred_taxons = pred_taxons[~pred_taxons['text'].str.contains("microsp")]
+
+    return [doc.char_span(int(start), int(end)) for start, end in \
+        [tuple(re.search('\d+ \d+', offset).group(0).split(' ')) \
+            for offset in pred_taxons.offsets.tolist()]]
+
+
+
 def is_overlapping_span(s1: spacy.tokens.span.Span, s2: spacy.tokens.span.Span) \
     -> bool:
+    """Return True if any part of one span, s1, overlaps with the other, s2.
+    """
+    if len(set(range(s1.start, s1.end + 1)) & \
+        set(range(s2.start, s2.end + 1))) > 0:
+        return True
+    
+    return False
+
+
+def format_predicted_microsp_and_hosts_str(microsp: List[spacy.tokens.span.Span],
+    hosts: List[spacy.tokens.span.Span]) -> str:
+    """
+    """
     pass
 
 
-def get_spans_from_predictions(pred_text: str) -> Tuple[spacy.tokens.span.Span]:
-    pass
-
-
-def get_microsporidia_from_text(txt: str) -> str:
+def get_microsporidia_and_hosts(txt: str) -> str:
     """Extract possible microsporidia species mentions from a string, txt.
     Return a semi-colon formatted string of microsporidia species and their spans
     within the text (when tokenized by spaCy).
@@ -114,35 +158,21 @@ def get_microsporidia_from_text(txt: str) -> str:
     Ex: Microsporidium sp. 1 (2, 5); Microsporidium sp. 2 (6, 9); ...
     """
     doc = get_spacy_cached_text(txt)
-    microsp_matches = []
-
-    for match in re.finditer(NEW_SPECIES, doc.text):
-        start, end = match.span()
-        span = doc.char_span(start, end)
-
-        if span is not None:
-            microsp_matches.append(f"{span.text} ({span.start},{span.end})")
+    microsporidia = get_microsporidia_from_doc(doc)
+    pred_taxons = get_taxonerd_taxons(doc)
     
-    return '; '.join(microsp_matches)
-
-
-def get_hosts_from_text(txt: str, pred_microsp: str) -> str:
-    """Extract possible microsporidia host species mentions from a string, txt,
-    by retrieving TaxoNERD predicted taxons that don't overlap with any predicted
-    microsporidia species.
+    # from hosts, add any predicted taxon that isn't a Microsporidia name
+    hosts = []
+    for taxon in pred_taxons:
+        # TODO - add in dependency parsing to evaluate whether a predicted taxon
+        # is a true Microsporidia host species
+        if not any([is_overlapping_span(taxon, microsp) or taxon.text == microsp.text \
+             for microsp in microsporidia]):
+            hosts.append(taxon)
     
-    Return a similar string as from get_microsporidia_from_text.
-    """
-    doc = get_spacy_cached_text(txt)
-    taxonerd_preds = taxonerd.find_in_text(txt)
-
-    # filter out rows where text in ('microsporidium', 'microsporidia') or where
-    # text is only one token long
-
-    # convert offsets to token indices to create spans
-    # turn host + microsporidia predictions into spans
-    # remove any host predictions that have overlapping spans with microsporidia
-    # return formatted string of remaining hosts and their span indices
+    # keep non-unique values for now, as we can use that to make weakly labelled
+    # host/microsporidia data.
+    return (microsporidia, hosts)
     
 
 ################################################################################
@@ -152,58 +182,7 @@ if __name__ == '__main__':
 
 ################################################################################
 
-## OLD FUNCTIONS, KEEPING FOR REFERENCE
-
-## Function for extracting new species names from titles/abstracts
-
-def get_new_microsp_species(txt: str) -> str:
-    """Predict new microsporidia species from a title + abstract.
-
-    txt: title + abstract for a microsporidia species paper
-
-    return: string of predicted species names, separated by '; '
-    """
-    new_sp = []
-    matches = re.finditer(NEW_SPECIES, txt)
-
-    for match in matches:
-        start, end = match.span()
-        # Remove new species indicator (ex: n. sp.) from predicted microsporidia
-        # species, getting only the species name itself
-        # Also remove excess trailing whitespaces
-        new_sp.append(
-            re.sub(NEW_SPECIES_INDICATORS, '', txt[start : end + 1]).strip()
-            )
-    
-    # do list to set conversion to only get unique strings from list
-    return '; '.join(list(set(new_sp)))
-
-################################################################################
-
-## Predict microsporidia names from titles + abstracts
-
-# Load in formatted dataframe of microsporidia species data, from
-# src/1_format_data/3_misc_cleanup.R
-microsp_data = pd.read_csv('../../data/manually_format_multi_species_papers.csv')
-
-# Predict microsporidia species names
-microsp_data = microsp_data.assign(
-    pred_species = lambda df: df['title_abstract'].map(
-        lambda txt: get_new_microsp_species(txt)
-    )
-)
-
-################################################################################
-
-## Predict hosts associated with microsporidia species
-
-# NOTE: I'm taking a really naive approach, where I'm assuming any identified
-#       species name that doesn't correspond to a microsporidia species is a host.
-#       I'm also not distinguishing between which hosts belong to which
-#       microsporidia species, if there's multiple microsporidia species in a
-#       paper.
-
-
+## OLD CODE, KEEPING FOR REFERENCE
 
 # Helper function for normalizing all predicted/recorded species names w/ GBIF,
 # so we can directly compare predicted and recorded species names
@@ -219,64 +198,3 @@ def get_gbif_normalized_name(sp: str) -> str:
         return gbif_hit['canonicalName']
     
     return sp  # no hits from GBIF search for species name, return as is
-
-
-# Write helper function for extracting Taxonerd predictions from text
-def predict_taxonerd_hosts(microsp: str, txt: str, taxonerd) -> str:
-    """Predict and extract taxonomic/species names for hosts from text, using the
-    TaxoNerd model.
-
-    NOTE: a lot of non-specific family names (ex: Simuliidae) are also captured
-
-    microsp: Predicted microsporidia species name(s) in paper
-    txt: Title + abstract for a microsporidia paper
-    return: string of predicted species, separated by '; '
-    """
-    taxonerd_pred = taxonerd.find_in_text(txt)
-
-    if not taxonerd_pred.empty:
-        # get list of unique species entity predictions from text
-        taxonerd_pred = list(set(taxonerd_pred['text'].tolist()))
-
-        # exclude predicted microsporidia species from final list of hosts, and
-        # also exclude entries that just say 'Microsporidia' or something
-        return '; '.join(filter(lambda s: not microsp in s and not \
-                                    re.search('[Mm]icrosp', s),
-                         taxonerd_pred))
-
-    return ''
-
-# Make predictions for microsporidia hosts
-# probably the most atrocious code i've written
-microsp_data['pred_hosts'] = \
-    [predict_taxonerd_hosts(*a) for a in tuple(zip(microsp_data['pred_species'],
-        microsp_data['title_abstract'], [taxonerd] * len(microsp_data)))]
-
-################################################################################
-
-## New approach for extracting microsporidia names: use microsporidia genuses
-## from NCBI to find microsporidia species amongst Taxonerd predictions
-
-microsp_genuses = pd.read_csv('../../data/microsp_genuses/microsporidia_genuses.tsv',
-                              sep='\t')['name'].tolist()
-
-genus_matcher = PhraseMatcher(nlp.vocab)
-genus_matcher.add('microsp_genus', [nlp(genus) for genus in microsp_genuses])
-
-def get_microsp_species(taxonerd_species: str) -> str:
-    """Docstring goes here.
-    """
-    taxonerd_species = [nlp(sp) for sp in taxonerd_species.split('; ')]
-    microsp_sp = []
-
-    for sp in taxonerd_species:
-        if genus_matcher(sp):
-            microsp_sp.append(sp.text)
-
-    return '; '.join(microsp_sp)
-
-microsp_data = microsp_data.assign(
-    pred_species_2 = lambda df: df['pred_hosts'].map(
-        lambda species: get_microsp_species(species), na_action='ignore'
-    )
-)
